d74287ad2bd324121ab600b103ef7ff3dcfd556a
Christoph Knittel <ck@cca.io>
2023-02-11 16:45:21 +0100

    Get templates from create-rescript-app package instead of external source

--- index.mjs
+++ index.mjs
@@ -4,12 +4,30 @@ import c from "ansi-colors";
 import enquirer from "enquirer";
 import path from "path";
 import fs from "fs";
+import { fileURLToPath } from "url";
 import { execSync } from "child_process";
 
-const templates = {
-  Basic: "https://github.com/rescript-lang/rescript-project-template.git",
-  "Next.js": "https://github.com/ryyppy/rescript-nextjs-template.git",
-};
+// Get __dirname in an ES6 module
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const templates = [
+  {
+    name: "rescript-template-vite",
+    message: "Vite",
+    hint: "Opinionated boilerplate for Vite, Tailwind and ReScript",
+  },
+  {
+    name: "rescript-template-nextjs",
+    message: "Next.js",
+    hint: "Opinionated boilerplate for Next.js, Tailwind and ReScript",
+  },
+  {
+    name: "rescript-template-basic",
+    message: "Basic",
+    hint: "Command line hello world app",
+  },
+];
 
 async function getParams() {
   return await enquirer.prompt([
@@ -23,23 +41,9 @@ async function getParams() {
       type: "select",
       name: "templateName",
       message: "Select a template",
-      choices: Object.keys(templates),
+      choices: templates,
     },
   ]);
-}
-
-function createProjectDir(projectName, projectPath) {
-  try {
-    fs.mkdirSync(projectPath);
-  } catch (err) {
-    if (err.code === "EEXIST") {
-      console.log(`The folder ${c.red(projectName)} already exist in the current directory.`);
-      console.log("Please try again with another name.");
-    } else {
-      console.log(err);
-    }
-    process.exit(1);
-  }
 }
 
 function houseKeeping(projectName, projectPath) {
@@ -60,10 +64,15 @@ async function main() {
   console.log("This tool will help you set up your new ReScript project quickly.\n");
 
   const { projectName, templateName } = await getParams();
-  console.log();
 
+  const templatePath = path.join(__dirname, "templates", templateName);
   const projectPath = path.join(process.cwd(), projectName);
-  createProjectDir(projectName, projectPath);
+
+  if (fs.existsSync(projectPath)) {
+    console.log(`The folder ${c.red(projectName)} already exist in the current directory.`);
+    console.log("Please try again with another name.");
+    process.exit(1);
+  }
 
   console.log(
     "Creating a new ReScript project",
@@ -71,9 +80,7 @@ async function main() {
   );
 
   try {
-    const repoUrl = templates[templateName];
-    execSync(`git clone --depth 1 ${repoUrl} ${projectPath}`);
-
+    fs.cpSync(templatePath, projectPath, { recursive: true });
     houseKeeping(projectName, projectPath);
 
     console.log(c.bold("Happy hacking!"));
