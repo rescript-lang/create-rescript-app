5e2aac18a1327f445f10e37e6d47ef0d27637636
Rob <illusionalsagacity@users.noreply.github.com>
2025-11-15 19:55:03 -0500

    fix: update vite + nextjs config to ignore rescript build artifacts

--- templates/rescript-template-vite/vite.config.js
+++ templates/rescript-template-vite/vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,4 +10,10 @@ export default defineConfig({
       include: ["**/*.res.mjs"],
     }),
   ],
+  server: {
+    watch: {
+      // We ignore ReScript build artifacts to avoid unnecessarily triggering HMR on incremental compilation
+      ignored: ["**/lib/bs/**", "**/lib/ocaml/**", "**/lib/rescript.lock"],
+    },
+  },
 });
